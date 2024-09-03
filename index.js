b92b7bdcb325d168c81fb2c75d17a1a9e4303b60
akshay <akshayprajapati424206@gmail.com>
2024-09-03 18:36:36 +0530

    array

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const name = 'akshay';
 
 const dob = '12/05/98'
 const is adult = () =>
-  if (age >= 18)
+  if {(age >= 18)
     return true;
-    else false;
+else false;
+}
+const arr = { 1, 2, 3, 4};
+
+
